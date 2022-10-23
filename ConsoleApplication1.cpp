995204a439464f32212683ebc49a7362ecc9efc9
chertousovapolina <115408269+chertousovapolina@users.noreply.github.com>
2022-10-23 16:02:04 +0300

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,18 @@
+﻿// Чертоусова 23.10.2022 Арифметическая прогрессия
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int A1, A2, N;
+    int S = 0;
+    cin >> A1 >> A2 >> N;
+    int D = A2 - A1;
+    S = A1 + (N - 1) * D;
+    cout << S;
+  }
+    
+
+
+
+
